4aeab84fc37f0e391edb56fb3952cac17146210d
lewmas9152 <smnjuguna22@gmail.com>
2024-07-21 19:57:47 +0300

    fix: fixed the issue with allocating each new employee  department and role

--- employee_management_backend/authenticator/serializers.py
+++ employee_management_backend/authenticator/serializers.py
@@ -19,9 +19,12 @@ class LeaveSerializer(serializers.ModelSerializer):
         fields = [ 'id', 'employee', 'leave_type', 'start_date', 'end_date', 'reason', 'status', 'created_at', 'updated_at']
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    department_id = serializers.PrimaryKeyRelatedField(queryset=Department.objects.all(),allow_null=True, required=False)
+    role_id = serializers.PrimaryKeyRelatedField(queryset=Role.objects.all(),allow_null=True, required=False)
     department = DepartmentSerializer(read_only=True)
     role = RoleSerializer(read_only=True)
 
     class Meta:
         model = Employee
-        fields = [ 'id', 'username', 'email', 'role', 'department', 'salary', 'phone', 'address', 'date_hired', 'updated_at']
+        fields = [ 'id', 'username', 'email', 'role', 'role_id', 'department','department_id', 'salary', 'phone', 'address', 'date_hired', 'updated_at']
+        depth = 1
